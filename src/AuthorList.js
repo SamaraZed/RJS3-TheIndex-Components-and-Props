4af42f46b2a47de64b6b3c82c6e684857e961f4f
Ayham Samara <Ayham.Samara@live.com>
2020-03-11 10:52:53 +0200

    React Task 2

---
+++ src/AuthorList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import authors from "./data";
+import AuthorCard from "./AuthorCard";
+
+const AuthorList = props => {
+  const authorCards = props.authors.map(author => (
+    <AuthorCard key={author.first_name + author.last_name} author={author} />
+  ));
+
+  return (
+    <div className="authors">
+      <h3>Authors</h3>
+      <div className="row">{authorCards}</div>
+    </div>
+  );
+};
+
+export default AuthorList;
